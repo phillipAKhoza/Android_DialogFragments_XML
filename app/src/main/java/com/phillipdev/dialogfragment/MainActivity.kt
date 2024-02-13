d90ca5e906dfe7373625bed255e0f7f37cef9daa
phillip khoza <phillipantonk@gmail.com>
2024-02-13 18:26:27 +0200

    add click listener to button

--- app/src/main/java/com/phillipdev/dialogfragment/MainActivity.kt
+++ app/src/main/java/com/phillipdev/dialogfragment/MainActivity.kt
@@ -2,10 +2,23 @@ package com.phillipdev.dialogfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnViewDialog : Button
+    lateinit var name: TextView
+    lateinit var age: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnViewDialog = findViewById(R.id.button)
+        name = findViewById(R.id.textViewName)
+        age = findViewById(R.id.textViewAge)
+
+        btnViewDialog.setOnClickListener {
+
+        }
     }
 }
