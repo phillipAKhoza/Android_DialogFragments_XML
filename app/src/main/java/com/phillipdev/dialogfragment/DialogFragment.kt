b0edbe6f6be759cc09278543118ff30c235600fe
phillip khoza <phillipantonk@gmail.com>
2024-02-13 18:53:23 +0200

    map variable to resources

--- app/src/main/java/com/phillipdev/dialogfragment/DialogFragment.kt
+++ app/src/main/java/com/phillipdev/dialogfragment/DialogFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 class DialogFragment : Fragment() {
 
-
+    lateinit var name : EditText
+    lateinit var age: EditText
+    lateinit var cancel : Button
+    lateinit var ok : Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view : View = inflater.inflate(R.layout.fragment_dialog, container, false)
+
+        name = view.findViewById(R.id.editName)
+        age = view.findViewById(R.id.editAge)
+        cancel = view.findViewById(R.id.btnCancel)
+        ok = view.findViewById(R.id.btnOk)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dialog, container, false)
+        return view
     }
 
 }
